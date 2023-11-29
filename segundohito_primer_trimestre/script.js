df0552ac1c45b40f46071296b7e9f665ecd44b89
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-11-29 10:37:27 +0000

    Update CV and website design

--- segundohito_primer_trimestre/script.js
+++ segundohito_primer_trimestre/script.js
@@ -12,3 +12,20 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
     console.log('CV page is loaded and ready.');
 });
+header.style.transform = 'translateY(-20px)';
+header.style.transition = 'opacity 1s ease-in-out, transform 1s ease-in-out';
+
+window.addEventListener('DOMContentLoaded', () => {
+    header.style.opacity = 1;
+    header.style.transform = 'translateY(0)';
+});
+
+// Add a slide-in effect to the form
+const form = document.querySelector('form');
+form.style.opacity = 0;
+form.style.transform = 'translateY(20px)';
+form.style.transition = 'opacity 1s ease-in-out, transform 1s ease-in-out';
+
+window.addEventListener('DOMContentLoaded', () => {
+    form.style.opacity = 1;
+    form.style.transform = 'translateY(0)';
