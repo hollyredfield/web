17aca334817cdb7294681b134e5263ecdf9f5f11
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-11-29 19:01:42 +0000

    Update contact form and CV page

--- segundohito_primer_trimestre/script.js
+++ segundohito_primer_trimestre/script.js
@@ -1,31 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Page is loaded and ready.');
-
-    // Add a simple animation to the video
-    var video = document.querySelector('video');
-    video.style.opacity = 0;
-    setTimeout(function() {
-        video.style.transition = 'opacity 2s';
-        video.style.opacity = 1;
-    }, 500);
+document.getElementById('title').addEventListener('click', function() {
+    this.style.color = this.style.color === 'blue' ? 'black' : 'blue';
 });
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('CV page is loaded and ready.');
-});
-header.style.transform = 'translateY(-20px)';
-header.style.transition = 'opacity 1s ease-in-out, transform 1s ease-in-out';
-
-window.addEventListener('DOMContentLoaded', () => {
-    header.style.opacity = 1;
-    header.style.transform = 'translateY(0)';
-});
-
-// Add a slide-in effect to the form
-const form = document.querySelector('form');
-form.style.opacity = 0;
-form.style.transform = 'translateY(20px)';
-form.style.transition = 'opacity 1s ease-in-out, transform 1s ease-in-out';
-
-window.addEventListener('DOMContentLoaded', () => {
-    form.style.opacity = 1;
-    form.style.transform = 'translateY(0)';
